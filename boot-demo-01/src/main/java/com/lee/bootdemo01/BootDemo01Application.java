05a3f5cb203b6af473afb2b6aa7f6e36199c2f7a
liyong403 <liyong18@jd.com>
2018-02-13 16:06:36 +0800

    first commit

---
+++ boot-demo-01/src/main/java/com/lee/bootdemo01/BootDemo01Application.java
@@ -0,0 +1,12 @@
+package com.lee.bootdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemo01Application.class, args);
+	}
+}
